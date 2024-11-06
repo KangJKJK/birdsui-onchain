1d9d05004e4cd5f72c5672072f0e6f297b7ca14f
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-11-06 23:39:16 +0900

    Update birdsui-onchain.sh

--- birdsui-onchain.sh
+++ birdsui-onchain.sh
@@ -82,7 +82,7 @@ case $choice in
             echo "  \"$line\","
         done
         echo "];"  # 배열 끝
-    } > "$WORK/config/proxiy_list.js"
+    } > "$WORK/app/config/proxiy_list.js"
 
     # SUI 지갑의 프라이빗키 입력 받기
     read -p "SUI 지갑의 프라이빗키를 입력하세요. 여러계정의 경우 쉼표로 구분하세요: " sui_private_key
